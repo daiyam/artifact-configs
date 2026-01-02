438a7410d827f459ac06e9522ac251c5859f007b
Baptiste Augrain <daiyam@zokugun.org>
2026-01-02 16:21:37 +0100

    feat(artifact-npm-cli-ts): improve base code

--- packages/npm-cli-ts/configs/src/cli.ts
+++ packages/npm-cli-ts/configs/src/cli.ts
@@ -1,6 +1,7 @@
 import { Command } from 'commander';
-import pkg from '../package.json';
+import pkg from '../package.json' with { type: 'json' };
 import { hello } from './commands/hello.js';
+import { run } from './run.js';
 
 const program = new Command();
 
@@ -8,10 +9,14 @@ program
 	.version(pkg.version, '-v, --version')
 	.description(pkg.description);
 
+// Option 1.
 program
 	.command('hello')
 	.description('print a greeting')
 	.argument('[name]')
 	.action(hello);
+
+// Option 2.
+program.action(run);
 
 program.parse();
---
+++ packages/npm-cli-ts/configs/src/run.ts
@@ -0,0 +1,4 @@
+import { type CliOptions } from './types.js';
+
+export async function run(options: CliOptions): Promise<void> {
+}
