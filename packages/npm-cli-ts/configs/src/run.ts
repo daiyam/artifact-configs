eef92dfce74191576f108423d4dad890d94674c9
Baptiste Augrain <daiyam@zokugun.org>
2026-01-08 17:51:33 +0100

    fix(artifact-npm-cli-ts): fix binary and patch; improve base src

--- packages/npm-cli-ts/configs/src/run.ts
+++ packages/npm-cli-ts/configs/src/run.ts
@@ -1,4 +1,11 @@
 import { type CliOptions } from './types.js';
+import * as logger from './utils/logger.js';
 
 export async function run(options: CliOptions): Promise<void> {
+	const start = Date.now();
+
+
+	const duration = Math.ceil((Date.now() - start) / 1000);
+
+	logger.finish(duration);
 }
