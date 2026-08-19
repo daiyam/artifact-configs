d0e9883bda7df46f2cfc209deb494e3e63fa0e8b
Baptiste Augrain <daiyam@zokugun.org>
2026-08-19 08:37:58 +0200

    enhance(artifact-lang-js): eslint

--- packages/lang-js/variants/20/configs/eslint.config.mjs
+++ packages/lang-js/variants/20/configs/eslint.config.mjs
@@ -1,5 +1,6 @@
 import { configure, gitignore, ignores } from '@zokugun/eslint-config';
 import { json, jsonc, yaml } from '@zokugun/eslint-config-data';
+import { glossary } from '@zokugun/eslint-config-glossary';
 import { javascript, regexp } from '@zokugun/eslint-config-js';
 import { markdown } from '@zokugun/eslint-config-md';
 import { nodejs } from '@zokugun/eslint-config-nodejs';
@@ -7,6 +8,7 @@ import { nodejs } from '@zokugun/eslint-config-nodejs';
 export default configure([
 	ignores(),
 	gitignore(),
+	glossary(),
 	markdown(),
 	nodejs(),
 	javascript(),
