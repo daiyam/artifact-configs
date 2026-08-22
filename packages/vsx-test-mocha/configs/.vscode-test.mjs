5664bc36eb15c2d67250e3e08381c2632e8693c1
Baptiste Augrain <daiyam@zokugun.org>
2026-08-22 09:00:33 +0200

    enhance(artifact-vsx-test-mocha): exclude fixtures

--- packages/vsx-test-mocha/configs/.vscode-test.mjs
+++ packages/vsx-test-mocha/configs/.vscode-test.mjs
@@ -1,9 +1,9 @@
-import { defineConfig } from '@vscode/test-cli'
+import { defineConfig } from '@vscode/test-cli';
 
 export default defineConfig({
 	files: '.test/test/**/*.test.js',
 	mocha: {
       ui: 'bdd',
-      timeout: 20000
+      timeout: 20000,
     },
-})
+});
--- packages/vsx-test-mocha/configs/eslint.config.mjs
+++ packages/vsx-test-mocha/configs/eslint.config.mjs
@@ -1,6 +1,7 @@
 import { mocha } from '@zokugun/eslint-config-test';
 
 export default configure([
+	ignores('test/fixtures/**'),
 	typescript(),
 	mocha(),
 	importX(),
