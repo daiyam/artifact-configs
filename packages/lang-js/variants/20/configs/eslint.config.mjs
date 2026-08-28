ca9062ed765e0c7066b15e9af08dd02acb960ac9
Baptiste Augrain <daiyam@zokugun.org>
2026-08-28 12:59:54 +0200

    enhance(artifact-lang-js): split into groups

--- packages/lang-js/variants/20/configs/eslint.config.mjs
+++ packages/lang-js/variants/20/configs/eslint.config.mjs
@@ -6,13 +6,28 @@ import { markdown } from '@zokugun/eslint-config-md';
 import { nodejs } from '@zokugun/eslint-config-nodejs';
 
 export default configure([
+	// configdotts/Ignore rules
 	ignores(),
 	gitignore(),
+
+	// configdotts/Documentation rules
 	glossary(),
 	markdown(),
+
+	// configdotts/Runtime rules
 	nodejs(),
+
+	// configdotts/Language rules
 	javascript(),
+
+	// configdotts/Language-feature rules
 	regexp(),
+
+	// configdotts/Testing rules
+
+	// configdotts/Sorting and stylistic rules
+
+	// configdotts/Data-format rules
 	json(),
 	jsonc(),
 	yaml(),
